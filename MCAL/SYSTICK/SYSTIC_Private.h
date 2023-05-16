e394d02b0e19021a2cfb36e8f68d4c66eec77b4a
Mahmoud Hamdi <128413333+M7moUD7AmdY@users.noreply.github.com>
2023-05-16 14:23:59 +0300

    Update SYSTIC_Private.h

--- MCAL/SYSTICK/SYSTIC_Private.h
+++ MCAL/SYSTICK/SYSTIC_Private.h
@@ -2,6 +2,10 @@
 #define SYSTICK_PRIVATE_H_
 
 #define STK_CTRL       (*(volatile u32*)(0xE000E010))
+#define COUNTFLAG    16
+#define CLKSOURCE    2
+#define TICKINT      1
+#define ENABLE       0
 #define STK_LOAD       (*(volatile u32*)(0xE000E014+0x04))
 #define STK_VAL        (*(volatile u32*)(0xE000E018+0x08))
 #define STK_CALIB      (*(volatile u32*)(0xE000E01C+0x0C))
